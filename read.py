b22ae285b07cd432b56c0e0f2a784abf3bad5715
LuizzDutra <luizzidutra@gmail.com>
2025-02-15 16:53:59 -0300

    programador_startup e view

--- read.py
+++ read.py
@@ -2,18 +2,31 @@ import streamlit as st
 import controller
 
 def get_all_tables():
-    for t in controller.get_tables():
-        st.write(t[1])
-        st.table(t[0])
+    tables = controller.get_tables()
+    for name, table in tables.items():
+        st.write(name)
+        st.table(table)
 
 def get_table(table_name):
-    if table_name == "--":
+    if table_name == "Nenhuma":
+        st.write()
+    elif table_name == "Todas":
         get_all_tables()
     else:
         st.write(table_name)
         st.table(controller.get_table(table_name))
 
+
+def get_consulta(consulta: str):
+    st.table(controller.make_query("""SELECT NOME_STARTUP, NOME_PROGRAMADOR FROM startup NATURAL LEFT JOIN programador_startup NATURAL LEFT JOIN Programador
+                                   WHERE NOME_PROGRAMADOR IS NOT NULL
+                                    GROUP BY NOME_STARTUP""").fetchall())
+
 def get_read_page():
-    select = st.selectbox("Tabela:", options=["--"] + controller.get_tables_names())
+    st.header("Tabelas")
+    select = st.selectbox("", options=["Nenhuma", "Todas"] + controller.get_tables_names())
     get_table(select)
+    st.header("Consultas")
+    select_consulta = st.selectbox("", options=["Programadores por Startup"])
+    get_consulta(select_consulta)
 
